a0701914e63924d2ab556e220a0ebbdf4749026c
lurx98 <lurx98@gmail.com>
2022-07-07 12:15:55 +0800

    登录获取保存token

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,19 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
-
+const TOKEN_KEY = 'USER'
 export default new Vuex.Store({
   state: {
+    user: JSON.parse(localStorage.getItem(TOKEN_KEY))
   },
   getters: {
   },
   mutations: {
+    setUser(state, data) {
+      state.user = data
+      // 为了防止刷新丢失，我们需要把数据备份到本地存储
+      localStorage.setItem(TOKEN_KEY, JSON.stringify(state.user))
+    }
   },
   actions: {
   },
